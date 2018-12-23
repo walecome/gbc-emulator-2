7ae0149ced15e6afc234c8c2a92a6e4b8b1fa92d
Alexander Wilkens <alexander.wilkens96@gmail.com>
2018-12-23 19:00:37 +0100

    Change opcode and byte type to uint8_t

--- src/Constants.hh
+++ src/Constants.hh
@@ -2,9 +2,10 @@
     Useful contants are defined in this file.
 */
 #pragma once
+#include <stdint.h>
 
-using opcode_t = unsigned char;
-using byte_t = unsigned char;
+using opcode_t = uint8_t;
+using byte_t = uint8_t;
 using opcode_function = void (*)();
 
 constexpr int NUMBER_OF_INSTRUCTIONS = 256;
