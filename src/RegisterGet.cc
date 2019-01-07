ee9e5d1fbc6c1881674cc075590792f545770428
Alexander Wilkens <alexander.wilkens96@gmail.com>
2019-01-07 20:47:32 +0100

    Reformat file with getters and setters

--- src/RegisterGet.cc
+++ src/RegisterGet.cc
@@ -1,126 +1,29 @@
 #include "Processor.hh"
 #include "Processor.hh"
-
 // 8bit
-
-Register8bit *Processor::getA()
-{
-    return A;
-}
-
-Register8bit *Processor::getB()
-{
-    return B;
-}
-
-Register8bit *Processor::getC()
-{
-    return C;
-}
-
-Register8bit *Processor::getD()
-{
-    return D;
-}
-
-Register8bit *Processor::getE()
-{
-    return E;
-}
-
-Register8bit *Processor::getF()
-{
-    return F;
-}
-
-Register8bit *Processor::getH()
-{
-    return H;
-}
-
-Register8bit *Processor::getL()
-{
-    return L;
-}
-
-register8_t Processor::getValueA()
-{
-    return A->getValue();
-}
-
-register8_t Processor::getValueB()
-{
-    return B->getValue();
-}
-
-register8_t Processor::getValueC()
-{
-    return C->getValue();
-}
-
-register8_t Processor::getValueD()
-{
-    return D->getValue();
-}
-
-register8_t Processor::getValueE()
-{
-    return E->getValue();
-}
-
-register8_t Processor::getValueF()
-{
-    return F->getValue();
-}
-
-register8_t Processor::getValueH()
-{
-    return H->getValue();
-}
-
-register8_t Processor::getValueL()
-{
-    return L->getValue();
-}
+Register8bit *Processor::getA() { return A; }
+Register8bit *Processor::getB() { return B; }
+Register8bit *Processor::getC() { return C; }
+Register8bit *Processor::getD() { return D; }
+Register8bit *Processor::getE() { return E; }
+Register8bit *Processor::getF() { return F; }
+Register8bit *Processor::getH() { return H; }
+Register8bit *Processor::getL() { return L; }
+register8_t Processor::getValueA() { return A->getValue(); }
+register8_t Processor::getValueB() { return B->getValue(); }
+register8_t Processor::getValueC() { return C->getValue(); }
+register8_t Processor::getValueD() { return D->getValue(); }
+register8_t Processor::getValueE() { return E->getValue(); }
+register8_t Processor::getValueF() { return F->getValue(); }
+register8_t Processor::getValueH() { return H->getValue(); }
+register8_t Processor::getValueL() { return L->getValue(); }
 
 // 16bit
-
-Register16bit *Processor::getAF()
-{
-    return AF;
-}
-
-Register16bit *Processor::getBC()
-{
-    return BC;
-}
-
-Register16bit *Processor::getDE()
-{
-    return DE;
-}
-
-Register16bit *Processor::getHL()
-{
-    return HL;
-}
-
-register16_t Processor::getValueAF()
-{
-    return AF->getValue();
-}
-
-register16_t Processor::getValueBC()
-{
-    return BC->getValue();
-}
-
-register16_t Processor::getValueDE()
-{
-    return DE->getValue();
-}
-
-register16_t Processor::getValueHL()
-{
-    return HL->getValue();
-}
+Register16bit *Processor::getAF() { return AF; }
+Register16bit *Processor::getBC() { return BC; }
+Register16bit *Processor::getDE() { return DE; }
+Register16bit *Processor::getHL() { return HL; }
+register16_t Processor::getValueAF() { return AF->getValue(); }
+register16_t Processor::getValueBC() { return BC->getValue(); }
+register16_t Processor::getValueDE() { return DE->getValue(); }
+register16_t Processor::getValueHL() { return HL->getValue(); }
--- src/RegisterSet.cc
+++ src/RegisterSet.cc
@@ -1,65 +1,16 @@
 #include "Processor.hh"
-
 // 8bit
-
-void Processor::setValueA(register8_t value)
-{
-    A->setValue(value);
-}
-
-void Processor::setValueB(register8_t value)
-{
-    B->setValue(value);
-}
-
-void Processor::setValueC(register8_t value)
-{
-    C->setValue(value);
-}
-
-void Processor::setValueD(register8_t value)
-{
-    D->setValue(value);
-}
-
-void Processor::setValueE(register8_t value)
-{
-    E->setValue(value);
-}
-
-void Processor::setValueF(register8_t value)
-{
-    F->setValue(value);
-}
-
-void Processor::setValueH(register8_t value)
-{
-    H->setValue(value);
-}
-
-void Processor::setValueL(register8_t value)
-{
-    L->setValue(value);
-}
+void Processor::setValueA(register8_t value) { A->setValue(value); }
+void Processor::setValueB(register8_t value) { B->setValue(value); }
+void Processor::setValueC(register8_t value) { C->setValue(value); }
+void Processor::setValueD(register8_t value) { D->setValue(value); }
+void Processor::setValueE(register8_t value) { E->setValue(value); }
+void Processor::setValueF(register8_t value) { F->setValue(value); }
+void Processor::setValueH(register8_t value) { H->setValue(value); }
+void Processor::setValueL(register8_t value) { L->setValue(value); }
 
 // 16bit
-
-void Processor::setValueAF(register16_t value)
-{
-    AF->setValue(value);
-}
-
-void Processor::setValueBC(register16_t value)
-{
-    BC->setValue(value);
-}
-
-void Processor::setValueDE(register16_t value)
-{
-    DE->setValue(value);
-}
-
-void Processor::setValueHL(register16_t value)
-{
-    HL->setValue(value);
-}
+void Processor::setValueAF(register16_t value) { AF->setValue(value); }
+void Processor::setValueBC(register16_t value) { BC->setValue(value); }
+void Processor::setValueDE(register16_t value) { DE->setValue(value); }
+void Processor::setValueHL(register16_t value) { HL->setValue(value); }
