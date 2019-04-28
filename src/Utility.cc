8f5b546614f5b80d4dfbe0d65947bd899bbfb841
Alexander Wilkens <alexander.wilkens96@gmail.com>
2019-04-28 15:03:21 +0200

    Add hexString utility function

--- src/Utility.cc
+++ src/Utility.cc
@@ -2,18 +2,30 @@
 
 namespace Util {
 
-void hexPrint(unsigned value, unsigned length, bool flush) {
-    std::cout << "0x" << std::setfill('0') << std::setw(length) << std::hex
-              << value;
-
-    if (flush) std::cout << std::endl;
-}
-
 std::ostream &operator<<(std::ostream &os,
                          const std::pair<register8_t, std::string> &p) {
     os << "(" << (unsigned)p.first;
     os << ", " << p.second << ")";
 
     return os;
+}
+
+void hexPrint(unsigned value, unsigned length, bool flush) {
+    std::cout << "0x" << std::setfill('0') << std::setw(length)
+              << std::uppercase << std::hex << value;
+
+    // Reset std::cout to decimal after std::hex
+    std::cout << std::dec;
+
+    if (flush) std::cout << std::endl;
+}
+
+std::string hexString(unsigned value, unsigned length) {
+    std::stringstream ss {};
+
+    ss << "0x" << std::setfill('0') << std::setw(length) << std::uppercase
+       << std::hex << value;
+
+    return ss.str();
 }
 }  // namespace Util
--- src/Utility.hh
+++ src/Utility.hh
@@ -2,6 +2,8 @@
 
 #include <iomanip>
 #include <iostream>
+#include <limits>
+#include <string>
 #include <utility>
 
 #include "Constants.hh"
@@ -10,5 +12,6 @@ namespace Util {
 std::ostream &operator<<(std::ostream &os,
                          const std::pair<register8_t, std::string> &p);
 void hexPrint(unsigned value, unsigned length, bool flush = false);
+std::string hexString(unsigned value, unsigned length);
 
 }  // namespace Util
